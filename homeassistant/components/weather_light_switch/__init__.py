becc07f8e0fa59ce591db5b2686feda39c396a43
axelhertzberg <axelher@chalmers.se>
2023-11-22 14:04:53 +0100

    [weather_light_switch] Added a weather service to the weather_light_switch
    * There is todos because there is now hardcoded lights, that will be changed
    * There is a todo to see if the weather feature is on or off.

--- homeassistant/components/weather_light_switch/__init__.py
+++ homeassistant/components/weather_light_switch/__init__.py
@@ -1,7 +1,14 @@
 """Registers listeners for state change events."""
 
+import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    ServiceResponse,
+    SupportsResponse,
+    callback,
+)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.event import (
     EventStateChangedData,
@@ -9,7 +16,7 @@ from homeassistant.helpers.event import (
 )
 from homeassistant.helpers.typing import ConfigType, EventType
 
-from .const import DOMAIN
+from .const import ATTR_SIM_LIGHT_ENTITY, BLACK, COLOR_MAP, DOMAIN, WINDY
 
 CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
@@ -45,6 +52,55 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 },
             )
 
+    async def _weather_service_call(call: ServiceCall) -> ServiceResponse:
+        # FM:See if the weather_feature is on
+        # If is off -> Act like normal light --> toggle
+        # If is on -> Set the light based on weather
+
+        # FM:Choose from the selected light entities that is selected in the switcher. Also do loop for all chosen ones
+        weather_ent_id = "weather.smhi_weather"
+        weather_state = hass.states.get(weather_ent_id)
+
+        weather_type = (
+            call.data["main"]
+            if call.data.get("main") is not None
+            else weather_state.state
+            if weather_state and weather_state.state is not None
+            else WINDY
+        )
+
+        # Define the RGB color based on your weather type (replace this with your logic)
+        rgb_color = COLOR_MAP.get(weather_type, BLACK)
+
+        # Fire custom event
+        hass.bus.async_fire(
+            "rgb_event", {"rgb": rgb_color, "condition": str(weather_type)}
+        )
+
+        # Call to turn the light on
+        await hass.services.async_call(
+            "light",
+            "turn_on",
+            {
+                "entity_id": ATTR_SIM_LIGHT_ENTITY,
+                "rgb_color": rgb_color,
+            },
+        )
+
+        return {"main": str(weather_type), "rgb": str(rgb_color)}
+
+    hass.services.async_register(
+        domain=DOMAIN,
+        service="weather_service",
+        service_func=_weather_service_call,
+        schema=vol.Schema(
+            {
+                vol.Optional("main"): str,
+            }
+        ),
+        supports_response=SupportsResponse.OPTIONAL,
+    )
+
     @callback
     async def update_lights_weather(event: EventType[EventStateChangedData]) -> None:
         weather = event.data["new_state"]
@@ -55,5 +111,4 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         change_color(color)
 
     async_track_state_change_event(hass, ["weather.smhi_home"], update_lights_weather)
-
     return True
--- homeassistant/components/weather_light_switch/const.py
+++ homeassistant/components/weather_light_switch/const.py
@@ -1,3 +1,56 @@
 """Constants for this integration."""
 
 DOMAIN = "weather_light_switch"
+
+ATTR_SIM_LIGHT_ENTITY = "light.simulated_light"
+ATTR_SMHI_WEATHER = "weather.smhi_weather"
+
+# Weather conditions as constants
+CLEAR_NIGHT = "clear-night"
+CLOUDY = "cloudy"
+EXCEPTIONAL = "exceptional"
+FOG = "fog"
+HAIL = "hail"
+LIGHTNING = "lightning"
+LIGHTNING_RAINY = "lightning-rainy"
+PARTLY_CLOUDY = "partlycloudy"
+POURING = "pouring"
+RAINY = "rainy"
+SNOWY = "snowy"
+SNOWY_RAINY = "snowy-rainy"
+SUNNY = "sunny"
+WINDY = "windy"
+WINDY_VARIANT = "windy-variant"
+OFF = "OFF"
+
+# RGB colors as constants
+NAVY_BLUE = (0, 0, 128)
+DARK_GRAY = (169, 169, 169)
+RED = (255, 0, 0)
+SILVER = (192, 192, 192)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+TEAL = (0, 128, 128)
+SNOW = (255, 250, 250)
+RED_ORANGE = (255, 69, 0)
+BLACK = (10, 0, 0)
+GRAY = (128, 128, 128)
+
+COLOR_MAP = {
+    CLEAR_NIGHT: NAVY_BLUE,  # Navy Blue
+    CLOUDY: DARK_GRAY,  # Dark Gray
+    EXCEPTIONAL: RED,  # Red
+    FOG: SILVER,  # Silver
+    HAIL: BLUE,  # Blue
+    LIGHTNING: YELLOW,  # Yellow
+    LIGHTNING_RAINY: TEAL,  # Teal
+    PARTLY_CLOUDY: YELLOW,  # Yellow
+    POURING: BLUE,  # Blue
+    RAINY: BLUE,  # Blue
+    SNOWY: SNOW,  # Snow
+    SNOWY_RAINY: BLUE,  # Blue
+    SUNNY: YELLOW,  # Yellow
+    WINDY: RED_ORANGE,  # Red-Orange
+    WINDY_VARIANT: RED_ORANGE,  # Red-Orange
+    OFF: GRAY,
+}
