409a98558615e3351fbd70a43e2461bdb736bf0e
axelhertzberg <axelher@chalmers.se>
2023-11-22 14:04:53 +0100

    [weather_light_switch] Added a weather service to the weather_light_switch
    * There is todos because there is now hardcoded lights, that will be changed
    * There is a todo to see if the weather feature is on or off.

--- homeassistant/components/weather_light_switch/__init__.py
+++ homeassistant/components/weather_light_switch/__init__.py
@@ -1,14 +1,75 @@
 """Registers listeners for state change events."""
 
+import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    ServiceResponse,
+    SupportsResponse,
+)
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
+from .const import ATTR_SIM_LIGHT_ENTITY, BLACK, COLOR_MAP, DOMAIN, WINDY
 
 CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Register event listeners during startup of Home Assistant."""
+
+    async def _weather_service_call(call: ServiceCall) -> ServiceResponse:
+        # FM:See if the weather_feature is on
+        # If is off -> Act like normal light --> toggle
+        # If is on -> Set the light based on weather
+
+        # FM:Choose from the selected light entities that is selected in the switcher. Also do loop for all chosen ones
+        weather_ent_id = "weather.smhi_weather"
+        weather_state = hass.states.get(weather_ent_id)
+
+        weather_type = (
+            call.data["main"]
+            if call.data.get("main") is not None
+            else weather_state.state
+            if weather_state and weather_state.state is not None
+            else WINDY
+        )
+
+        # Define the RGB color based on your weather type (replace this with your logic)
+        rgb_color = COLOR_MAP.get(weather_type, BLACK)
+
+        # Fire custom event
+        hass.bus.async_fire(
+            "rgb_event", {"rgb": rgb_color, "condition": str(weather_type)}
+        )
+
+        # Call to turn the light on
+        await hass.services.async_call(
+            "light",
+            "turn_on",
+            {
+                "entity_id": ATTR_SIM_LIGHT_ENTITY,
+                "rgb_color": rgb_color,
+            },
+        )
+
+        return {"main": str(weather_type), "rgb": str(rgb_color)}
+
+    hass.services.async_register(
+        domain=DOMAIN,
+        service="weather_service",
+        service_func=_weather_service_call,
+        schema=vol.Schema(
+            {
+                vol.Optional("main"): str,
+            }
+        ),
+        supports_response=SupportsResponse.OPTIONAL,
+    )
+    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
