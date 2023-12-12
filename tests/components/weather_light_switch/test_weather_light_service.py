e35e8a181f2199e9dc596657e350726e9258068b
michaeldaang <78927304+michaeldaang@users.noreply.github.com>
2023-12-12 18:31:00 +0000

    Test that different weather types corresponds to certain colors

--- tests/components/weather_light_switch/test_weather_light_service.py
+++ tests/components/weather_light_switch/test_weather_light_service.py
@@ -1,4 +1,8 @@
 """File for test weather light service."""
+from homeassistant.components.weather_light_switch.weather_mapping import (
+    get_color_for_weather_state,
+    rgb_to_hs,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
@@ -9,3 +13,21 @@ async def test_has_weather_service(hass: HomeAssistant):
 
     # Assert if the switch has the service
     assert hass.services.has_service("switch", "weather_service")
+
+
+async def test_colors():
+    """Test that different weather types corresponds to certain colors."""
+    test_cases = [
+        {"weather_state": "windy", "expected_color": (255, 69, 0)},
+        {"weather_state": "rainy", "expected_color": (30, 144, 255)},
+        {"weather_state": "cloudy", "expected_color": (0, 128, 128)},
+        {"weather_state": "sunny", "expected_color": (255, 255, 0)},
+    ]
+
+    for test_cases in test_cases:
+        weather_state = test_cases["weather_state"]
+        expected_color = test_cases["expected_color"]
+        expected_color_hs = rgb_to_hs(expected_color)
+
+        actual_color = get_color_for_weather_state(weather_state)
+        assert actual_color == expected_color_hs
