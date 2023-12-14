976ed487b98f30671f030c3d47cb844b5edb811e
Kenny Bang <kennybaaang@gmail.com>
2023-12-14 18:12:07 +0100

    Test for color extractor
    Co-authored-by: Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>

--- tests/components/music_light_switch/test_music_light_switch.py
+++ tests/components/music_light_switch/test_music_light_switch.py
@@ -1,4 +1,5 @@
 """Tests for the music light switch."""
+from homeassistant.components.music_light_switch.switch import color_extractor
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
@@ -9,3 +10,12 @@ async def test_music_switch(hass: HomeAssistant):
 
     # Assert 3 because it uses http, mediaplayer and music light switch
     assert len(hass.config_entries.hass.data.get("integrations")) == 3
+
+
+def test_light(hass: HomeAssistant):
+    """Test color extractor."""
+    with open("testimage.png", "rb") as image_file:
+        image = image_file.read()
+        # bytes= binascii.hexlify(image).decode("utf-8")
+
+    assert color_extractor(image) == (252, 68, 4)
