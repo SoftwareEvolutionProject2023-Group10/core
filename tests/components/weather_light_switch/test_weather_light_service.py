e14c4035d345d5fbfa4c05549cacb7f6088db996
Benjamin Moosherr <benjamin.moosherr@uni-ulm.de>
2023-12-14 16:48:19 +0000

    Test correct weather_service calls on config entry updates

--- tests/components/weather_light_switch/test_weather_light_service.py
+++ tests/components/weather_light_switch/test_weather_light_service.py
@@ -152,3 +152,68 @@ async def test_light_change(hass: HomeAssistant):
     assert result["main"] == WEATHER_CONDITION
     assert result["switch_state"] == STATE_ON
     assert result["light_ids"] == [LIGHT_ENTITY_ID]
+
+
+async def test_config_entry_update(hass: HomeAssistant):
+    """Test that the switch updates when the config entry is changed."""
+    await async_setup_component(hass, DOMAIN, {})
+
+    WEATHER_ENTITY_ID = "weather.test1"
+    LIGHT_ENTITY_ID = "light.test1"
+    config_entry = config_entries.ConfigEntry(
+        1,
+        DOMAIN,
+        "Mock Title",
+        {},
+        "test",
+        options={
+            "weather_entity_id": WEATHER_ENTITY_ID,
+            "light_ids": [LIGHT_ENTITY_ID],
+        },
+    )
+
+    await async_setup_entry(hass, config_entry)
+    SWITCH_ENTITY_ID = "switch.weather_light_switch_enabled"
+
+    weather_service_calls = async_mock_service(hass, SWITCH_DOMAIN, WEATHER_SERVICE)
+    await hass.async_block_till_done()
+
+    # Change the config entry
+    WEATHER_ENTITY_ID = "weather.test2"
+    LIGHT_ENTITY_ID = "light.test2"
+    hass.config_entries.async_update_entry(
+        config_entry,
+        options={
+            "weather_entity_id": WEATHER_ENTITY_ID,
+            "light_ids": [LIGHT_ENTITY_ID],
+        },
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get(SWITCH_ENTITY_ID).state == STATE_OFF
+    assert len(weather_service_calls) == 0
+
+    # Turn the switch on
+    await hass.services.async_call(
+        SWITCH_DOMAIN,
+        SERVICE_TURN_ON,
+        {ATTR_ENTITY_ID: SWITCH_ENTITY_ID},
+        blocking=True,
+    )
+    await hass.async_block_till_done()
+    assert hass.states.get(SWITCH_ENTITY_ID).state == STATE_ON
+    assert len(weather_service_calls) == 1
+
+    # Change the config entry
+    WEATHER_ENTITY_ID = "weather.test3"
+    LIGHT_ENTITY_ID = "light.test3"
+    hass.config_entries.async_update_entry(
+        config_entry,
+        options={
+            "weather_entity_id": WEATHER_ENTITY_ID,
+            "light_ids": [LIGHT_ENTITY_ID],
+        },
+    )
+
+    await hass.async_block_till_done()
+    assert hass.states.get(SWITCH_ENTITY_ID).state == STATE_ON
+    assert len(weather_service_calls) == 2
